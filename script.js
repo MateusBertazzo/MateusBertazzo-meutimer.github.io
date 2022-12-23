00715315ce82c0ba11553105666765525dec33a4
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-12-23 10:53:20 -0300

    adiciona variaveis no escopo global

---
+++ script.js
@@ -0,0 +1,6 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let seconds = 0;
+let timer;
