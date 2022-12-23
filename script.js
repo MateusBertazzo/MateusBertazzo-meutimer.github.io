3ac28e961e8fd22f349bbd045e6875ae1dbeb10d
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-12-23 10:54:06 -0300

    adiciona function getTimeFronSeconds

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function getTimeFronSecond(segundos) {
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'GMT'
+  });
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
